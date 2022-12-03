07433117f6c2ba888c0344224d2a9a594de59459
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-12-03 18:29:02 +0800

    Integrate database

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Subject, Schedule, Faculty, Availability, Course)
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('course_id', 'code', 'title', 'units',
+                    'duration_minutes',  'year_number')
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('subject_id', 'block_number', 'week_day', 'day_time')
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'employment_status',
+                    'schedule_id', 'total_units', 'course_id')
+
+
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('faculty_id', 'week_day', 'start_time', 'end_time')
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'chairperson_id', 'college')
+
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(Faculty, FacultyAdmin)
+admin.site.register(Availability, AvailabilityAdmin)
+admin.site.register(Course, CourseAdmin)
--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+EMPLOYMENT_TYPE_CHOICES = (
+    ('PT', 'Part Time'),
+    ('FT', 'Full Time'),
+)
+
+WEEK_DAY_CHOICES = (
+    ('MON', 'Monday'),
+    ('TUE', 'Tuesday'),
+    ('WED', 'Wednesday'),
+    ('THU', 'Thursday'),
+    ('FRI', 'Friday'),
+    ('SAT', 'Saturday'),
+    ('SUN', 'Sunday'),
+)
+
+COLLEGE_CHOICES = (
+    ('CET', 'College of Engineering and Technology'),
+)
+
+COURSE_CHOICES = (
+    ('BSCS', 'Bachelor of Science in Computer Science'),
+
+    ('BSCHE', 'Bachelor of Science in Chemical Engineering'),
+
+    ('BSCE', 'Bachelor of Science in Civil Engineering'),
+)
+
+
+class Course(models.Model):
+    title = models.CharField(choices=COURSE_CHOICES,
+                             max_length=20, unique=True)
+    chairperson_id = models.OneToOneField(
+        User, on_delete=models.CASCADE, unique=True)
+    college = models.CharField(choices=COLLEGE_CHOICES, max_length=40)
+
+    def __str__(self):
+        return self.title
+
+
+class Subject(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    code = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    units = models.IntegerField()
+    duration_minutes = models.TimeField()
+    year_number = models.IntegerField()
+
+
+class Schedule(models.Model):
+    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    block_number = models.IntegerField()
+    week_day = models.CharField(choices=WEEK_DAY_CHOICES, max_length=3)
+    day_time = models.TimeField()
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+    employment_status = models.CharField(
+        choices=EMPLOYMENT_TYPE_CHOICES, max_length=10)
+    schedule_id = models.ForeignKey(
+        Schedule, on_delete=models.CASCADE, blank=True, null=True)
+    total_units = models.IntegerField(blank=True, null=True)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Availability(models.Model):
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    week_day = models.CharField(choices=WEEK_DAY_CHOICES, max_length=3)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
