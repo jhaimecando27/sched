62bfc8b680100e40a2ff5770878ca43099bf5c26
SPARDA-REQUIEM <patrickbuyain@gmail.com>
2022-12-04 15:23:17 +0800

    new file:   app/main/forms.py
    	modified:   app/main/views.py
    	modified:   app/templates/home/faculty-form.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import facultyForm
 
 
 # Ref 1: https://stackoverflow.com/questions/2245895/is-there-a-simple-way-to-get-group-names-of-a-user-in-django
@@ -8,7 +9,7 @@ def home(request):
 
     return render(request, 'home/home.html', {})
 
-
 @login_required
 def prof(request):
     return render(request, 'home/faculty-form.html', {})
+
