33cced88870900367c90248a663da1264c68b09c
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-12-04 11:50:46 +0800

    fix(model): git error messages
    - Dito kaya may mga '<<<<<<< HEAD' para mahiwalay yung 2 codes kasi diba
      yung ginawa mo i-naccept yung both changes, which is dapat tlga. pero
      yung mga yung '<<<<<<< HEAD' is yun nga guide lng tlga para sa dev, at
      kailangan manual ayusin yun.
    - cinomment out ko muna yung faculty form kasi parang dapat nasa
      separate file sya. Kasi nakalagay sya dito sa `models.py` at lahat ng
      changes dito sa `models.py` nilalagay agad sa database, eh diba meron
      na tayong database? So, magkakagulo. So form na to is for the UI para
      double check yung inputs nila, kasi bawal tayo mag tiwala sa inputs ng
      user, so better kung nasa separate file sya, at may library yung
      Django regarding sa form.
    
      Django form doc: https://docs.djangoproject.com/en/4.1/topics/forms/#more-on-fields

--- app/main/models.py
+++ app/main/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
-# Create your models here.
-class facultyMemberForm(models.Model):
-    inputId = models.CharField(max_length = 10)
-    inputName = models.CharField(max_length = 100)
-    inputEmploymentStatus = models.CharField(max_length = 100)
-    inputCollege = models.CharField(max_length = 100)
-    inputDepartment = models.CharField(max_length = 100)
-    inputExpertiseSubject = models.CharField(max_length = 300) #Checkbox 
-    inputDayAvailability = models.CharField(max_length = 100) #Checkbox With InputField Below | Calendar
-    inputTimePreference = models.CharField(max_length = 100) #Checkbox With InputField Below | Calendar
+# class facultyMemberForm(models.Model):
+#     inputId = models.CharField(max_length = 10)
+#     inputName = models.CharField(max_length = 100)
+#     inputEmploymentStatus = models.CharField(max_length = 100)
+#     inputCollege = models.CharField(max_length = 100)
+#     inputDepartment = models.CharField(max_length = 100)
+#     inputExpertiseSubject = models.CharField(max_length = 300) #Checkbox
+#     inputDayAvailability = models.CharField(max_length = 100) #Checkbox With InputField Below | Calendar
+#     inputTimePreference = models.CharField(max_length = 100) #Checkbox With InputField Below | Calendar
+#
+#     def __str__(self):
+#         return self.inputName
 
-    def __str__(self):
-        return self.inputName
-=======
 EMPLOYMENT_TYPE_CHOICES = (
     ('PT', 'Part Time'),
     ('FT', 'Full Time'),
@@ -89,4 +87,3 @@ class Availability(models.Model):
     week_day = models.CharField(choices=WEEK_DAY_CHOICES, max_length=3)
     start_time = models.TimeField()
     end_time = models.TimeField()
->>>>>>> database
