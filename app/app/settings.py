25e6da39af0a9f377dda8a57e8e4108d4ea9aa1c
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2023-01-08 23:39:32 +0800

    fix(Email Account): Create email account w/ app password for sending email

--- app/app/settings.py
+++ app/app/settings.py
@@ -129,11 +129,10 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Custom backend (to override default settings)
 # Mailing
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'no.reply.jhaime@gmail.com'
-EMAIL_HOST_PASSWORD = 'nyxmzxfwtfgbpcvq'
+EMAIL_HOST_USER = 'class.schedule.no.reply@gmail.com'
+EMAIL_HOST_PASSWORD = 'cyvzkvlgpwbduccg'
